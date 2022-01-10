d5296ab06690ffe3985c7d2da5d26c634b932380
Hannah McCullough <hmac091@gmail.com>
2022-01-10 10:58:30 -0700

    commit tree 64571e73908e83cad1383c0f527bfcd5dae07464

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+
+    number/2
+
+    # another way to write the above code
+    # return nil unless number.class == Integer
+end
+
